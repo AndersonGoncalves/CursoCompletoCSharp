b2e478ac9fc82c2d566a26c1ca49ea6fba7306a2
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2019-05-11 11:35:17 -0300

    Conclusao do exercicio 3

--- ProvaLogicaProgramacao/Program.cs
+++ ProvaLogicaProgramacao/Program.cs
@@ -5,17 +5,9 @@ namespace ProvaLogicaProgramacao {
     class Program {
         static void Main(string[] args) {
 
-            #region Exercício 01
-
-            //Fazer um programa para ler o código de uma peça 1,
-            //o número de peças 1, o valor unitário de cada peça 1,
-            //o código de uma peça 2, o número de peças 2 e o valor
-            //unitário de cada peça 2.Calcule e mostre o valor a ser pago.
-            //Entrada: 12 1 5.30
-            //Entrada: 16 2 5.10 
-            //Saída: VALOR A PAGAR: R$ 15.50
-
-            Console.WriteLine("Exercício 01");
+            #region PARTE 1: ESTRUTURA SEQUENCIAL 
+            //Exercício 1.1:
+            Console.WriteLine("Exercício 1.1");
             Console.WriteLine("Digite o Código / quantidade / valor da primeira peça (separado por espaço): ");
             string[] peca1 = Console.ReadLine().Split(' ');
 
@@ -24,23 +16,37 @@ namespace ProvaLogicaProgramacao {
 
             double valorPagar = (int.Parse(peca1[1]) * double.Parse(peca1[2])) + (int.Parse(peca2[1]) * double.Parse(peca2[2]));
             Console.WriteLine("VALOR A PAGAR: {0}", valorPagar.ToString("F2", CultureInfo.InvariantCulture));
-            #endregion
 
-
-            #region Exercício 02
-
-            //Faça um programa para ler o valor do raio de um círculo,
-            //e depois mostrar o valor da área deste círculo com quatro casas
-            //decimais conforme exemplos. Fórmula: Area = PI*raio*raio
-            //Entrada: 2.00
-            //Saída: A=12.5664 
-
+            //Exercício 1.2:
             Console.WriteLine("-----------------------------------------");
-            Console.WriteLine("Exercício 02");
+            Console.WriteLine("Exercício 1.2");
             Console.WriteLine("Informe o valor do raio:");
             double raio = double.Parse(Console.ReadLine());
             double area = Math.PI * Math.Pow(raio, 2);
             Console.WriteLine($"A={area.ToString("F4", CultureInfo.InvariantCulture)}");
+            #endregion
+
+            #region PARTE 2: ESTRUTURA CONDICIONAL 
+            //Exercício 2.1:
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Exercício 2.1");
+            Console.WriteLine("Digite o Código / quantidade (separado por espaço): ");
+            string[] pecas = Console.ReadLine().Split(' ');
+
+            double ValorDaPeca = 0;
+            if (int.Parse(pecas[0]) == 1)
+                ValorDaPeca = 4.00; 
+            else if (int.Parse(pecas[0]) == 2)
+                ValorDaPeca = 4.50;
+            else if (int.Parse(pecas[0]) == 3)
+                ValorDaPeca = 5.00;
+            else if (int.Parse(pecas[0]) == 4)
+                ValorDaPeca = 2.00;
+            else if (int.Parse(pecas[0]) == 5)
+                ValorDaPeca = 1.50;
+
+            double valorFinal = ValorDaPeca * double.Parse(pecas[1]);
+            Console.WriteLine($"Total: R$ {valorFinal.ToString("F2", CultureInfo.InvariantCulture)}");
             #endregion
         }
     }
