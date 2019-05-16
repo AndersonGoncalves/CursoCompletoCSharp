6e794b39ce951606399834dd70e7bdfcadd81010
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2019-05-16 13:23:20 -0300

    Inicio do exercicio de polimorfismo

---
+++ Polimorfismo/Entities/UsedProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+
+namespace Polimorfismo.Entities
+{
+    public class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+        public UsedProduct(string name, double price, DateTime manufactureDate ) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+        public sealed override string PriceTag()
+        {
+            StringBuilder sb = new StringBuilder();
+            //sb.AppendLine("PRICE TAGS:");
+            sb.AppendLine($"{Name}(Used) ${Price.ToString("F2", CultureInfo.InvariantCulture)} (Manufacture date: ${ManufactureDate.ToString("dd/MM/yyyy")}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of products!");
+        }
+    }
+}
