718f60246fd64529ea1e5a25e6803ac93a3702c6
AndersonGoncalves <anndersonn.gonncalves@gmail.com>
2019-06-04 16:30:57 -0300

    Inicio do exercicio sobre arquivos (File, FileInfo, etc...)

---
+++ Arquivos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Copiando um arquivo
+            try
+            {
+                string arquivoOrigem = @"D:\fileOrigem.txt";
+                string arquivoDestino = @"D:\fileDestino.txt";
+
+                FileInfo fileInfo = new FileInfo(arquivoOrigem);
+                fileInfo.CopyTo(arquivoDestino);
+                //ou somente => File.Copy(arquivoOrigem, arquivoDestino);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+            #endregion
+
+
+            #region Percorrer um arquivo
+            try
+            {
+                string arquivo = @"D:\file.txt";
+                using (StreamReader streamReader = File.OpenText(arquivo))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        string linha = streamReader.ReadLine();
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+
+            #endregion
+
+        }
+    }
+}
