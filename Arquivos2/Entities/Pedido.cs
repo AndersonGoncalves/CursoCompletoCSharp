6bc191d5aea5d5232fa1d5f3658fd5b9b9b5aa68
AndersonGoncalves <anndersonn.gonncalves@gmail.com>
2019-06-05 14:17:20 -0300

    Conclusao do exercicio de leitura e gravacao de arquivos

---
+++ Arquivos2/Entities/Pedido.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Arquivos2.Entities
+{
+    public class Pedido
+    {
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        public int Quantidade { get; set; }
+        
+        public Pedido(string nome, double valor, int quantidade)
+        {
+            Nome = nome;
+            Valor = valor;
+            Quantidade = quantidade;
+        }
+        public double ValorTotal()
+        {
+            return Valor * Quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + ValorTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Arquivos2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Arquivos2.Entities;
+using System.Globalization;
+
+namespace Arquivos2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arquivo = @"D:\Pedido.txt";
+            string arquivoDestino = @"D:\arquivoDestino.txt";
+            List<Pedido> Pedidos = new List<Pedido>();
+
+            try
+            {
+                string[] linhas = File.ReadAllLines(arquivo);
+                using (StreamWriter sw = File.AppendText(arquivoDestino))
+                {
+                    foreach (string linha in linhas)
+                    {
+                        string[] colunas = linha.Split(',');
+                        double valor = double.Parse(colunas[1], CultureInfo.InvariantCulture);
+                        Pedidos.Add(new Pedido(colunas[0], valor, int.Parse(colunas[2].ToString())));
+                    }
+
+                    foreach (Pedido pedido in Pedidos)
+                        sw.WriteLine(pedido);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+    }
+}
