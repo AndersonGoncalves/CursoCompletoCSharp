bf5e4f1a29a42688c17f1085497ad09b7c4b4997
Anderson Gonçalves <=>
2019-06-10 19:49:55 -0300

    Extension methods

---
+++ ExtensionMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace System
+{
+    public static class StringExtensions
+    {
+        public static string Abreviar(this string thisObj, int count)
+        {
+            if (thisObj.Length <= count)
+                return thisObj;
+            else
+                return thisObj.Substring(0, count) + "...";
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string heloWorld = "Hello World!";
+            Console.WriteLine(heloWorld.Abreviar(5));
+        }
+    }
+}
