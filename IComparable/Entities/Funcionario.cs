1bdd161ea7ab5fa25067fcd98fdd1c82ec203edd
Anderson Gonçalves <=>
2019-06-10 20:15:19 -0300

    IComparable

---
+++ IComparable/Entities/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace IComparable.Entities
+{
+    public class Funcionario : IComparable<Funcionario>
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario() {
+        }
+        public int CompareTo(Funcionario other)
+        {
+            if (Nome != other.Nome)
+                return Nome.CompareTo(other.Nome);
+            else
+                return Salario.CompareTo(other.Salario);
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ IComparable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using IComparable.Entities;
+using System.Collections.Generic;
+
+namespace IComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> lista = new List<Funcionario>()
+            {
+                new Funcionario { Nome = "Maria", Salario = 5000.00 },
+                new Funcionario { Nome = "Marcos", Salario = 4000.00 },
+                new Funcionario { Nome = "Marcos", Salario = 3000.00 },
+                new Funcionario { Nome = "Paulo", Salario = 3000.00 },
+                new Funcionario { Nome = "André", Salario = 7000.00 },
+                new Funcionario { Nome = "Alves", Salario = 7000.00 }
+            };
+
+            lista.Sort();
+
+            foreach (Funcionario funcionario in lista)
+                Console.WriteLine(funcionario);
+        }
+    }
+}
